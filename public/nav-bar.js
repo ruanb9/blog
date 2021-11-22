567f943c06c885abcba8cfe277a34116cd60d282
ruanb9 <56460504+ruanb9@users.noreply.github.com>
2021-11-22 18:57:43 -0300

    primeiro commit

---
+++ public/nav-bar.js
@@ -0,0 +1,8 @@
+function abrirMenu(){
+    document.getElementById('menu1').style.width="250px";
+    document.getElementById('conteudo').style.marginLeft="250px";
+}
+function fecharMenu(){
+  document.getElementById('menu1').style.width="0px";
+  document.getElementById('conteudo').style.marginLeft="0px";
+}
